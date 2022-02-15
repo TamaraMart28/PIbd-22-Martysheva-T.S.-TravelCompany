49a765586394e144322e19274b090fbbd7ced9de
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2022-02-15 18:46:10 +0400

    BusinessLogic и Contracts

---
+++ TravelCompany/TravelCompanyContracts/BindingModels/CompanyBindingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelCompanyContracts.BindingModels
+{
+    public class CompanyBindingModel
+    {
+        public int? Id { get; set; }
+        public string CompanyName { get; set; }
+        public string NameResponsible { get; set; }
+        public DateTime DateCreate { get; set; }
+        public Dictionary<int, (string, int)> CompanyConditions { get; set; }
+    }
+}
---
+++ TravelCompany/TravelCompanyContracts/StoragesContracts/ICompanyStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelCompanyContracts.BindingModels;
+using TravelCompanyContracts.ViewModels;
+
+namespace TravelCompanyContracts.StoragesContracts
+{
+    public interface ICompanyStorage
+    {
+        List<CompanyViewModel> GetFullList();
+        List<CompanyViewModel> GetFilteredList(CompanyBindingModel model);
+        CompanyViewModel GetElement(CompanyBindingModel model);
+        void Insert(CompanyBindingModel model);
+        void Update(CompanyBindingModel model);
+        void Delete(CompanyBindingModel model);
+    }
+}
