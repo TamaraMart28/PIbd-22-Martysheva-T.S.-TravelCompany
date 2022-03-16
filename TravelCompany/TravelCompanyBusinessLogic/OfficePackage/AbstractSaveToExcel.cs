870f9a2b394ac123ce451f469a3e0eca003013b6
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2022-03-16 13:18:39 +0400

    excel и путевки с условиями

---
+++ TravelCompany/TravelCompanyBusinessLogic/OfficePackage/AbstractSaveToExcel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelCompanyBusinessLogic.OfficePackage.HelperEnums;
+using TravelCompanyBusinessLogic.OfficePackage.HelperModels;
+
+namespace TravelCompanyBusinessLogic.OfficePackage
+{
+    public abstract class AbstractSaveToExcel
+    {
+		// Создание отчета
+		public void CreateReport(ExcelInfo info)
+		{
+			CreateExcel(info);
+
+			InsertCellInWorksheet(new ExcelCellParameters
+			{
+				ColumnName = "A",
+				RowIndex = 1,
+				Text = info.Title,
+				StyleInfo = ExcelStyleInfoType.Title
+			});
+
+			MergeCells(new ExcelMergeParameters
+			{
+				CellFromName = "A1",
+				CellToName = "C1"
+			});
+
+			uint rowIndex = 2;
+			foreach (var tc in info.TravelConditions)
+			{
+				InsertCellInWorksheet(new ExcelCellParameters
+				{
+					ColumnName = "A",
+					RowIndex = rowIndex,
+					Text = tc.TravelName,
+					StyleInfo = ExcelStyleInfoType.Text
+				});
+				rowIndex++;
+
+				foreach (var condition in tc.Conditions)
+				{
+					InsertCellInWorksheet(new ExcelCellParameters
+					{
+						ColumnName = "B",
+						RowIndex = rowIndex,
+						Text = condition.Item1,
+						StyleInfo = ExcelStyleInfoType.TextWithBroder
+					});
+
+					InsertCellInWorksheet(new ExcelCellParameters
+					{
+						ColumnName = "C",
+						RowIndex = rowIndex,
+						Text = condition.Item2.ToString(),
+						StyleInfo = ExcelStyleInfoType.TextWithBroder
+					});
+					rowIndex++;
+				}
+
+				InsertCellInWorksheet(new ExcelCellParameters
+				{
+					ColumnName = "C",
+					RowIndex = rowIndex,
+					Text = tc.TotalCount.ToString(),
+					StyleInfo = ExcelStyleInfoType.Text
+				});
+				rowIndex++;
+			}
+
+			SaveExcel(info);
+		}
+
+		// Создание excel-файла
+		protected abstract void CreateExcel(ExcelInfo info);
+
+		// Добавляем новую ячейку в лист
+		protected abstract void InsertCellInWorksheet(ExcelCellParameters excelParams);
+
+		// Объединение ячеек
+		protected abstract void MergeCells(ExcelMergeParameters excelParams);
+
+		// Сохранение файла
+		protected abstract void SaveExcel(ExcelInfo info);
+	}
+}
---
+++ TravelCompany/TravelCompanyBusinessLogic/OfficePackage/HelperModels/ExcelInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelCompanyContracts.ViewModels;
+
+namespace TravelCompanyBusinessLogic.OfficePackage.HelperModels
+{
+    public class ExcelInfo
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public List<ReportTravelConditionViewModel> TravelConditions { get; set; }
+    }
+}
