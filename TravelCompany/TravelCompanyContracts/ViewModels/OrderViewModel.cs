992deb38e9cf4d42de20af3a00e30770fb60d4b9
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2022-05-15 12:01:23 +0400

    fix

--- TravelCompany/TravelCompanyContracts/ViewModels/OrderViewModel.cs
+++ TravelCompany/TravelCompanyContracts/ViewModels/OrderViewModel.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel;
 using TravelCompanyContracts.Attributes;
-using System.Runtime.Serialization;
 
 namespace TravelCompanyContracts.ViewModels
 {
